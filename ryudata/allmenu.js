6e1f6a8501d245ffec81ac62ae0c987ed5ffa398
R-TEAM94 <fandyyydev@gmail.com>
2022-03-20 17:20:45 +0700

    V4

---
+++ ryudata/allmenu.js
@@ -0,0 +1,285 @@
+const fs = require('fs')
+const chalk = require('chalk')
+
+exports.allmenu = (prefix) => {
+	return`Apakah Kamu Tau?
+Tak Ada Yang Spesial Di Dunia Ini Kecuali Martabak Spesial
+
+*👤 OWNER MENU*
+⚘👾 ${prefix}setmenu
+⚘👾 ${prefix}join [link]
+⚘👾 ${prefix}leave
+⚘👾 ${prefix}self
+⚘👾 ${prefix}public
+⚘👾 ${prefix}setppbot
+⚘👾 ${prefix}block @user
+⚘👾 ${prefix}unblock @user
+⚘👾 ${prefix}bc
+⚘👾 ${prefix}bcgc
+⚘👾 ${prefix}setcmd [reply sticker/pesan]
+⚘👾 ${prefix}delcmd [reply sticker/pesan]
+⚘👾 ${prefix}listcmd
+⚘👾 ${prefix}lockcmd
+⚘👾 ${prefix}promosi
+
+*👥 GROUP MENU*
+⚘👾 ${prefix}antilink
+⚘👾 ${prefix}antiwame
+⚘👾 ${prefix}antivirtex
+⚘👾 ${prefix}nsfw
+⚘👾 ${prefix}afk [alasan]
+⚘👾 ${prefix}tagall
+⚘👾 ${prefix}hidetag
+⚘👾 ${prefix}absen
+⚘👾 ${prefix}hadir
+⚘👾 ${prefix}tidakhadir
+⚘👾 ${prefix}cekabsen
+⚘👾 ${prefix}hapusabsen
+⚘👾 ${prefix}linkgroup
+⚘👾 ${prefix}revoke
+⚘👾 ${prefix}ephemeral [option]
+⚘👾 ${prefix}setppgc
+⚘👾 ${prefix}setname [text]
+⚘👾 ${prefix}group [option]
+⚘👾 ${prefix}add @user
+⚘👾 ${prefix}kick @user
+⚘👾 ${prefix}promote @user
+⚘👾 ${prefix}demote @user
+
+*📦 DOWNLOAD MENU*
+⚘👾 ${prefix}tiktok [url]
+⚘👾 ${prefix}tiktokwm [url]
+⚘👾 ${prefix}tiktoknowm [url]
+⚘👾 ${prefix}tiktokaudio [url]
+⚘👾 ${prefix}instagram [url]
+⚘👾 ${prefix}twitter [url]
+⚘👾 ${prefix}facebook [url]
+⚘👾 ${prefix}ytmp4 [url]
+⚘👾 ${prefix}ytmp3 [url]
+⚘👾 ${prefix}play [judul]
+⚘👾 ${prefix}ytsearch [judul]
+⚘👾 ${prefix}gitclone
+
+*🔎 SEARCH MENU*
+⚘👾 ${prefix}pinterest [query]
+⚘👾 ${prefix}pantun
+⚘👾 ${prefix}google [query]
+⚘👾 ${prefix}gimage [query]
+⚘👾 ${prefix}kbbi [query]
+⚘👾 ${prefix}igstalk [username]
+⚘👾 ${prefix}ghstalk [username]
+
+*🗞️ INFORMASI MENU*
+⚘👾 ${prefix}infogempa
+⚘👾 ${prefix}faktaunik
+⚘👾 ${prefix}ssweb
+
+*✍️ NULIS MENU*
+⚘👾 ${prefix}nulisbiasa
+⚘👾 ${prefix}nuliskanan
+⚘👾 ${prefix}nuliskiri
+⚘👾 ${prefix}foliokanan
+⚘👾 ${prefix}foliokiri
+
+*👭 ASUPAN MENU*
+⚘👾 ${prefix}hijaber
+⚘👾 ${prefix}cecan
+⚘👾 ${prefix}bocil
+⚘👾 ${prefix}ukhty
+⚘👾 ${prefix}gheayubi
+⚘👾 ${prefix}rikagusriani
+⚘👾 ${prefix}santuy
+
+*👩 CECAN MENU*
+⚘👾 ${prefix}china
+⚘👾 ${prefix}vietnam
+⚘👾 ${prefix}thailand
+⚘👾 ${prefix}indonesia
+⚘👾 ${prefix}korea
+⚘👾 ${prefix}japan
+⚘👾 ${prefix}malaysia
+
+*🎙️ VOICE CHANGER*
+⚘👾 ${prefix}bass
+⚘👾 ${prefix}blown
+⚘👾 ${prefix}deep
+⚘👾 ${prefix}earrape
+⚘👾 ${prefix}fast
+⚘👾 ${prefix}fat
+⚘👾 ${prefix}nightcore
+⚘👾 ${prefix}reverse
+⚘👾 ${prefix}robot
+⚘👾 ${prefix}slow
+⚘👾 ${prefix}tupai
+
+*📸 EPHOTO MENU*
+⚘👾 ${prefix}3dnature
+⚘👾 ${prefix}bevel
+⚘👾 ${prefix}burnpaper
+⚘👾 ${prefix}quotesgrass
+⚘👾 ${prefix}stars
+⚘👾 ${prefix}flaming
+⚘👾 ${prefix}romance
+⚘👾 ${prefix}gerbang
+⚘👾 ${prefix}fur
+⚘👾 ${prefix}funnycup
+
+*🖼️ PHOTOOXY MENU*
+⚘👾 ${prefix}3d-underwater  
+⚘👾 ${prefix}3dtext-pig  
+⚘👾 ${prefix}3dvalentine-cards  
+⚘👾 ${prefix}anonymous-neon  
+⚘👾 ${prefix}advanced-glow  
+⚘👾 ${prefix}art-shader  
+⚘👾 ${prefix}angels-wings 
+
+*🎮 FUN MENU*
+⚘👾 ${prefix}truth
+⚘👾 ${prefix}dare
+⚘👾 ${prefix}bagaimanakah [teks]
+⚘👾 ${prefix}kapankah [teks]
+⚘👾 ${prefix}dimanakah [teks]
+⚘👾 ${prefix}suit [✌️/✊/✋]
+⚘👾 ${prefix}aduayam [🐥/🐤🐓/🐣/🦃]
+⚘👾 ${prefix}suitpvp [tag user]
+⚘👾 ${prefix}ttt
+⚘👾 ${prefix}delttt
+
+*🎰 GAME MENU*
+⚘👾 ${prefix}tebak lagu
+⚘👾 ${prefix}tebak gambar
+⚘👾 ${prefix}tebak kata
+⚘👾 ${prefix}tebak kalimat
+⚘👾 ${prefix}tebak lirik
+⚘👾 ${prefix}tebak lontong
+⚘👾 ${prefix}kuismath
+
+*🎡 RANDOM MENU*
+⚘👾 ${prefix}darkjokes
+⚘👾 ${prefix}meme
+⚘👾 ${prefix}quotes
+⚘👾 ${prefix}quotes2
+⚘👾 ${prefix}katabijak
+⚘👾 ${prefix}wallgaming
+⚘👾 ${prefix}wallpentol
+⚘👾 ${prefix}wallcogan
+⚘👾 ${prefix}wallcecan
+⚘👾 ${prefix}wallestetik
+⚘👾 ${prefix}wallislami
+⚘👾 ${prefix}wallbkackpink
+⚘👾 ${prefix}wallpaperanime
+
+*⛩️ ANIME MENU*
+⚘👾 ${prefix}quotesanime
+⚘👾 ${prefix}wallpaper
+⚘👾 ${prefix}waifu
+⚘👾 ${prefix}loli
+⚘👾 ${prefix}cry
+⚘👾 ${prefix}kill
+⚘👾 ${prefix}hug
+⚘👾 ${prefix}pat
+⚘👾 ${prefix}lick
+⚘👾 ${prefix}kiss
+⚘👾 ${prefix}bite
+⚘👾 ${prefix}yeet
+⚘👾 ${prefix}neko
+⚘👾 ${prefix}bully
+⚘👾 ${prefix}bonk
+⚘👾 ${prefix}wink
+⚘👾 ${prefix}poke
+⚘👾 ${prefix}nom
+⚘👾 ${prefix}slap
+⚘👾 ${prefix}smile
+⚘👾 ${prefix}wave
+⚘👾 ${prefix}awoo
+⚘👾 ${prefix}blush
+⚘👾 ${prefix}smug
+⚘👾 ${prefix}glomp
+⚘👾 ${prefix}happy
+⚘👾 ${prefix}dance
+⚘👾 ${prefix}cringe
+⚘👾 ${prefix}cuddle
+⚘👾 ${prefix}highfive
+⚘👾 ${prefix}shinobu
+⚘👾 ${prefix}megumin
+⚘👾 ${prefix}handhold
+
+*🔞 NSFW MENU*
+⚘👾 ${prefix}ahegeo
+⚘👾 ${prefix}ass
+⚘👾 ${prefix}bdsm
+⚘👾 ${prefix}blowjob
+⚘👾 ${prefix}cuckold 
+⚘👾 ${prefix}cum 
+⚘👾 ${prefix}ero
+⚘👾 ${prefix}femdom
+⚘👾 ${prefix}foot
+⚘👾 ${prefix}gangbang
+⚘👾 ${prefix}glasses
+⚘👾 ${prefix}hentai
+⚘👾 ${prefix}ahy
+⚘👾 ${prefix}mstb
+⚘👾 ${prefix}neko
+⚘👾 ${prefix}orgy
+⚘👾 ${prefix}panties
+⚘👾 ${prefix}pussy
+⚘👾 ${prefix}yuri
+⚘👾 ${prefix}zettai
+
+*🎨 MAKER MENU*
+⚘👾 ${prefix}toimage
+⚘👾 ${prefix}smeme
+⚘👾 ${prefix}smeme2
+⚘👾 ${prefix}sticker
+⚘👾 ${prefix}tovideo
+⚘👾 ${prefix}togif
+⚘👾 ${prefix}tourl
+⚘👾 ${prefix}emojimix 😎+😂
+⚘👾 ${prefix}readmore
+⚘👾 ${prefix}ttp [teks]
+⚘👾 ${prefix}semoji [emoji]
+
+*👨‍🎨 TEXTPRO MENU*
+⚘👾 ${prefix}serti1 [teks]
+⚘👾 ${prefix}serti2 [teks]
+⚘👾 ${prefix}serti3 [teks]
+⚘👾 ${prefix}blackpink [teks]
+⚘👾 ${prefix}neon [teks]
+⚘👾 ${prefix}magma [teks]
+⚘👾 ${prefix}3dstone [teks]
+⚘👾 ${prefix}halloween [teks]
+⚘👾 ${prefix}horror [teks]
+⚘👾 ${prefix}larva [teks]
+⚘👾 ${prefix}toxic [teks]
+⚘👾 ${prefix}marvel [teks|teks]
+⚘👾 ${prefix}potter [teks]
+⚘👾 ${prefix}pornhub [teks|teks]
+⚘👾 ${prefix}tahta [teks]
+
+*☕ OTHER MENU*
+⚘👾 ${prefix}simi [teks]
+⚘👾 ${prefix}ping
+⚘👾 ${prefix}owner
+⚘👾 ${prefix}creator
+⚘👾 ${prefix}menu / ${prefix}help / ${prefix}?
+⚘👾 ${prefix}delete
+⚘👾 ${prefix}sc
+⚘👾 ${prefix}tqto
+⚘👾 ${prefix}donasi
+⚘👾 ${prefix}rules
+⚘👾 ${prefix}sewabot
+⚘👾 ${prefix}listgc
+⚘👾 ${prefix}liston
+⚘👾 ${prefix}liston
+⚘👾 ${prefix}report
+⚘👾 ${prefix}req
+`
+}
+
+let file = require.resolve(__filename)
+fs.watchFile(file, () => {
+	fs.unwatchFile(file)
+	console.log(chalk.redBright(`Update ${__filename}`))
+	delete require.cache[file]
+	require(file)
+})
---
+++ ryudata/promosi.js
@@ -0,0 +1,39 @@
+exports.promosi1 = () => {
+	return`Ready Vps/Rdp Murah
+
+Minat Chat
+wa.me/6285609233482
+`
+}
+
+exports.promosi2 = () => {
+	return`Ready Lonte Murah
+
+Minat Chat
+wa.me/6285609233482
+`
+}
+
+exports.promosi3 = () => {
+	return`Ready Cewe Murah
+
+Minat Chat
+wa.me/6285609233482
+`
+}
+
+exports.promosi4 = () => {
+	return`Ready Akun Murah
+
+Minat Chat
+wa.me/6285609233482
+`
+}
+
+exports.promosi5 = () => {
+	return`Ready Meki Murah
+
+Minat Chat
+wa.me/6285609233482
+`
+}
