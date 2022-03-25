d764c7b66153a697564545e60ed5a2a3a21b4e68
R-TEAM94 <97998325+R-TEAM94@users.noreply.github.com>
2022-03-26 06:04:29 +0700

    Add files via upload

---
+++ settings.js
@@ -0,0 +1,56 @@
+const fs = require('fs')
+const chalk = require('chalk')
+
+//===> FREE API
+global.APIs = {
+	ryuu: 'https://api-ryubotz.herokuapp.com',
+}
+
+global.APIKeys = {
+	'https://api-ryubotz.herokuapp.com': 'RyuBotz',
+}
+
+//===> UBAH DISINI
+global.namaowner = 'Ryuuka Team' //Ganti Jadi Namamu
+global.owner = ['6285609233482','6283891921787'] //Kalo Mau Lebih Tambahin Aja
+global.nomerowner = '6285609233482' //Owner Utama Istilahnya
+global.namabot = 'RyuBotz MD' //Ganti Jadi Nama Botmu
+global.packname = '© Ryubotz MD' //Sticker Weem
+global.author = 'Ryubotz By Ryuuka Team' //Sticker Weem
+global.webme = 'https://api-ryubotz.herokuapp.com'
+global.sessionName = 'session'
+global.bg_welcome = 'https://telegra.ph/file/528b594d5d91c08482515.jpg' //Max 30kb Agar Tidak Delay
+global.youtube = 'https://youtube.com/c/RyuukaBotz'
+global.github = 'https://github.com/R-TEAM94'
+global.instagram = 'https://instagram.com/fandyyy._'
+
+//===> BACKGROUND SETIAP MENU
+global.bg1 = 'https://www6.flamingtext.com/net-fu/proxy_form.cgi?&imageoutput=true&script=water-logo&doScale=true&scaleWidth=800&scaleHeight=500&fontsize=100&text='
+global.bg2 = 'https://www6.flamingtext.com/net-fu/proxy_form.cgi?&imageoutput=true&script=monarch-logo&doScale=true&scaleWidth=800&scaleHeight=500&fontsize=100&text='
+global.bg3 = 'https://www6.flamingtext.com/net-fu/proxy_form.cgi?&imageoutput=true&script=yolo-logo&doScale=true&scaleWidth=800&scaleHeight=500&fontsize=100&text='
+global.bg4 = 'https://www6.flamingtext.com/net-fu/proxy_form.cgi?&imageoutput=true&script=fun-run-logo&doScale=true&scaleWidth=800&scaleHeight=500&fontsize=100&text='
+
+global.prefa = ['','!','/','#','.']
+global.sp = '⭔'
+
+//===> EDIT SESUAI KEMAUANMU
+global.mess = {
+admin: '*LU ADMIN? 🤨*',
+botAdmin: '*BOT BUKAN ADMIN 😔*',
+owner: '*LU BUKAN OWNER GW 😠*',
+group: '*KHUSUS GROUP 😏*',
+private: '*KHUSUS PRIVATE CHAT*',
+bot: '*KHUSUS PENGGUNA NOMER BOT*',
+wait: '*SABAR SEDANG DI PROSES ...*',
+done: '*DONE NGAB 🤗*',
+}
+global.thumb = fs.readFileSync('./media/ryuu.jpg')
+    
+//===> INI GAUSAH DIUBAH
+let file = require.resolve(__filename)
+fs.watchFile(file, () => {
+fs.unwatchFile(file)
+console.log(chalk.redBright(`Update'${__filename}'`))
+delete require.cache[file]
+require(file)
+})
